ccc3415ef5ac1f6f33cdd17c173207c491483bc4
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 02:59:48 -0600

    Agregar archivos de proyecto.

---
+++ Instrucciones_Goto/Program.cs
@@ -0,0 +1,13 @@
+﻿//Continue
+for (int id = 0; id <= 10; id++)
+{
+    Thread.Sleep(800);
+    if (id == 8)
+    {
+        goto Liquidacion;
+    }
+    Console.WriteLine(id);
+}
+Liquidacion:
+Console.WriteLine("Se envio al departamento de contabilidad...");
+Console.ReadKey(true);
